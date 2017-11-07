caf3a1a1d9d9f15974107cc129b8e7708f337db7
Kunxian <670370379@qq.com>
2017-11-07 08:54:39 +0800

    123

--- Account/Login.aspx.cs
+++ Account/Login.aspx.cs
@@ -4,21 +4,75 @@ using System;
 using System.Web;
 using System.Web.UI;
 using Education_SYS;
-
+using System.Data;
+using System.Configuration;
+using System.Linq;
+using System.Web.Security;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Xml.Linq;
+using System.Data.SqlClient;
+using System.Web.Configuration;
+using System.Collections.Generic;
 public partial class Account_Login : Page
 {
         protected void Page_Load(object sender, EventArgs e)
         {
+           
             RegisterHyperLink.NavigateUrl = "Register";
             var returnUrl = HttpUtility.UrlEncode(Request.QueryString["ReturnUrl"]);
-            if (!String.IsNullOrEmpty(returnUrl))
+           if (!String.IsNullOrEmpty(returnUrl))
             {
+                Response.Write("<script> alert(" + 123 + ")</script>");
                 RegisterHyperLink.NavigateUrl += "?ReturnUrl=" + returnUrl;
+                
             }
         }
 
         protected void LogIn(object sender, EventArgs e)
         {
+            Response.Write("<script>alert(登录)</script>");
+            String  _connectionString = WebConfigurationManager.ConnectionStrings["Edu"].ConnectionString;
+            SqlConnection con = new SqlConnection(_connectionString);
+            SqlCommand cmd = new SqlCommand("SELECT vUserName,vPassword,vName from Teacher where vUserName=@vUserName and vPassword=@vPassword", con);
+            cmd.Parameters.AddWithValue("@vUserName", UserName.Text);
+            cmd.Parameters.AddWithValue("@vPassword", Password.Text);
+            
+            Response.Write("<script>alert("+(string)UserName.Text+"@"+(string)Password.Text +")</script>");
+            int result;
+            SqlDataReader reader;
+            using (con)
+            {
+                con.Open();
+                reader = cmd.ExecuteReader();
+                reader.Read();
+               // result =cmd.ExecuteNonQuery();
+                try
+                {
+                    Response.Write("<script>alert(" + (string)reader["vPassword"] + ")</script>");
+                    //string msg = "登录成功";
+                  //  Response.Write("<script>alert('" + msg + "')</script>");
+                    HttpCookie cookie = new HttpCookie("MyCook");//初使化并设置Cookie的名称
+                    DateTime dt = DateTime.Now;
+                    TimeSpan ts = new TimeSpan(0, 0, 1, 0, 0);//过期时间为1分钟
+                    cookie.Expires = dt.Add(ts);//设置过期时间
+                    cookie.Values.Add("Name", (string)reader["vName"]);
+                     Response.AppendCookie(cookie);
+                     Response.Redirect("myWeb");
+                }
+                catch
+                {
+                    string msg = "登录失败,请输入正确的帐号和密码";
+                    Response.Write("<script>alert('"+msg+"')</script>");
+                }
+             
+               con.Close();
+            }
+            
+           
+            
+            /*
             if (IsValid)
             {
                 // Validate the user password
@@ -34,6 +88,6 @@ public partial class Account_Login : Page
                     FailureText.Text = "Invalid username or password.";
                     ErrorMessage.Visible = true;
                 }
-            }
+            }*/
         }
 }
